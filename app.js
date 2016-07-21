00dfc9cbdda274199c2c544f1a0aa5c0363cda00
Brooks Patton <brooks.patton@galvanize.com>
2016-07-21 10:42:46 -0600

    cannot send an empty object

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.get('/api/v1/health', (req, res, next)=>{
 });
 
 app.post('/api/v1/sum', (req, res, next)=>{
+  if(!req.body.type) {
+    return res.status(400).json({
+      status: 400,
+      code: 1,
+      error: 'bad payload',
+      message: 'The payload was incorrect. Pleas verify that you are sending the correct looking object and try again.'
+    });
+  }
+
+  next();
+});
+
+app.post('/api/v1/sum', (req, res, next)=>{
 
   let sum = req.body.num1 + req.body.num2;
 
--- test/sum-spec.js
+++ test/sum-spec.js
@@ -8,6 +8,23 @@ let should = chai.should();
 let api = supertest(app);
 
 describe('Sending a POST to /api/v1/sum', ()=>{
+  describe('should fail', ()=>{
+    it('when not passed an object', (done)=>{
+      api.post('/api/v1/sum')
+        .send()
+        .expect(400)
+        .end((err, res)=>{
+          if(err) return done(err);
+
+          res.body.status.should.be.equal(400);
+          res.body.code.should.be.equal(1);
+          res.body.error.should.be.equal('bad payload');
+          res.body.message.should.be.equal('The payload was incorrect. Pleas verify that you are sending the correct looking object and try again.');
+
+          done();
+        })
+    })
+  })
   describe('should succeed', ()=>{
     it('in adding two numbers together', (done)=>{
       api.post('/api/v1/sum')
@@ -19,7 +36,7 @@ describe('Sending a POST to /api/v1/sum', ()=>{
         .expect(200)
         .end((err, res)=>{
           if(err) return done(err);
-          
+
           res.body.result.should.be.equal(13);
 
           done();
